8ba0443ca6243826cb28c0c93e5d9ff06c1003fe
Eric Gregory <egregory3@my.apsu.edu>
2020-05-06 19:51:58 -0500

    Added permissions check for location permissions

--- app/src/main/java/com/esquared/restaurantroulette/TheRules.java
+++ app/src/main/java/com/esquared/restaurantroulette/TheRules.java
@@ -1,8 +1,12 @@
 package com.esquared.restaurantroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -13,7 +17,10 @@ ImageButton button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_rules);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(TheRules.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        ;
         //Setup button
         button = findViewById(R.id.btn_rouletteWheel);
         button.setOnClickListener(new View.OnClickListener() {
